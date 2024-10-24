2c53fc4eff7244005e7998b70e0763bf9cb67f90
Busuioc Petru Claudiu <busuioc.p.claudiu@gmail.com>
2024-10-24 19:34:09 +0300

    sa traiesti emanuel

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 'use strict';
 
-const menuModalEl = document.querySelector('.menu-modal');
-const playBtn = document.querySelector('.play-button');
-const tutorialBtn = document.querySelector('.tutorial-button');
+const modalMenuEl = document.querySelector('.modal-menu-container');
+const menuPlayBtn = document.querySelector('.menu-play-button');
+const menuTutorialBtn = document.querySelector('.menu-tutorial-button');
 
 const overlayEl = document.querySelector('.overlay');
+
+const tutorialContainerEl = document.querySelector('.tutorial-container');
+const goBackBtn = document.querySelector('.go-back-button');
 
 // NOTE: make a function that takes 2 params,
 // applying hidden to the first one
@@ -18,16 +21,38 @@ const overlayEl = document.querySelector('.overlay');
 // actually maybe have more params so i can control yes/no more dynamically
 // contains(class) maybe
 // tutorialBtn.addEventListener('click', () => {
-//   menuModalEl.classList.add('hidden');
+//   modalMenuEl.classList.add('hidden');
 //   overlayEl.classList.remove('hidden');
 //   overlayEl.classList.add('visible');
 //   console.log('HELLO OVERLAY');
 // });
 
 // nesting things inside OVERLAY is NOT a good idea
-tutorialBtn.addEventListener('click', () => {
-  hideAndShow(menuModalEl, overlayEl);
+
+// should keep overlay toggling separate from hiding/showing
+menuTutorialBtn.addEventListener('click', () => {
+  toggleOverlay();
+  hideAndShow(modalMenuEl, tutorialContainerEl);
 });
+goBackBtn.addEventListener('click', () => {
+  toggleOverlay();
+  hideAndShow(tutorialContainerEl, modalMenuEl);
+});
+// helper function
+
+function toggleOverlay() {
+  if (overlayEl.classList.contains('visible')) {
+    overlayEl.classList.remove('visible');
+    overlayEl.classList.add('hidden');
+  } else if (overlayEl.classList.contains('hidden')) {
+    overlayEl.classList.remove('hidden');
+    overlayEl.classList.add('visible');
+  }
+}
+// problem here because instructionsContainer has flex, not block
+// NOTE: having flex on body kills the flow
+// possible solution: have a container for the "screen" u r moving to
+// so there is no need for flex handling below ????
 function hideAndShow(elToHide, elToShow, hideClass = 'hidden', showClass = 'visible') {
   elToHide.classList.remove(showClass);
   elToHide.classList.add(hideClass);
